5e5fa3d757953806092cc946b935bb8e6160cfff
David H. Rogers <dhr@lanl.gov>
2024-05-14 13:56:32 -0600

    updating to latest version

---
+++ examples/theater/ColumnSelect.py
@@ -0,0 +1,48 @@
+import pycinema
+import pycinema.filters
+import pycinema.theater
+import pycinema.theater.views
+
+# pycinema settings
+PYCINEMA = { 'VERSION' : '2.1.0'}
+
+# filters
+CinemaDatabaseReader_0 = pycinema.filters.CinemaDatabaseReader()
+ImageReader_0 = pycinema.filters.ImageReader()
+TableView_0 = pycinema.filters.TableView()
+ImageView_0 = pycinema.filters.ImageView()
+
+# properties
+CinemaDatabaseReader_0.inputs.path.set("data/sphere.cdb", False)
+CinemaDatabaseReader_0.inputs.file_column.set("FILE", False)
+ImageReader_0.inputs.table.set(CinemaDatabaseReader_0.outputs.table, False)
+ImageReader_0.inputs.file_column.set("FILE", False)
+ImageReader_0.inputs.cache.set(True, False)
+TableView_0.inputs.table.set(ImageReader_0.outputs.images, False)
+TableView_0.inputs.selection.set([1, 2, 3, 4, 5, 6], False)
+ImageView_0.inputs.images.set(TableView_0.outputs.table, False)
+ImageView_0.inputs.selection.set([], False)
+
+# layout
+tabFrame1 = pycinema.theater.TabFrame()
+splitFrame1 = pycinema.theater.SplitFrame()
+splitFrame1.setHorizontalOrientation()
+view1 = pycinema.theater.views.NodeEditorView()
+splitFrame1.insertView( 0, view1 )
+splitFrame1.setSizes([1018])
+tabFrame1.insertTab(0, splitFrame1)
+tabFrame1.setTabText(0, 'Layout 1')
+splitFrame2 = pycinema.theater.SplitFrame()
+splitFrame2.setHorizontalOrientation()
+view2 = pycinema.theater.views.FilterView( TableView_0 )
+splitFrame2.insertView( 0, view2 )
+view7 = pycinema.theater.views.FilterView( ImageView_0 )
+splitFrame2.insertView( 1, view7 )
+splitFrame2.setSizes([506, 505])
+tabFrame1.insertTab(1, splitFrame2)
+tabFrame1.setTabText(1, 'Layout 2')
+tabFrame1.setCurrentIndex(1)
+pycinema.theater.Theater.instance.setCentralWidget(tabFrame1)
+
+# execute pipeline
+CinemaDatabaseReader_0.update()
